177e97b89dcb661a4fb92759884edb893d7b58db
Enoch Belawu <enochbelawu@gmail.com>
2022-09-16 18:25:38 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var hamBurger = document.querySelector(".menu");
+var mobilemenu = document.querySelector(".mobile-menu");
+
+hamBurger.addEventListener("click", function(){
+    mobilemenu.classList.toggle("mobile-menu-toggle")
+})
+
